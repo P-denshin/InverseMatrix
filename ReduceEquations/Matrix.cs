196a3eb30ea4eb0c6328e6bd4eca0c42ba8b49df
Secretary <s1300640@sendai-nct.jp>
2014-08-12 23:47:19 +0900

    暫定的完成　入力要素が多い場合はどうする？

--- ReduceEquations/Matrix.cs
+++ ReduceEquations/Matrix.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-
 namespace ReduceEquations {
     class Matrix {
         public Fraction[,] Value;
         int col, row;
-        
         #region プロパティ
         /// <summary>
         /// 行数（１～）
@@ -15,29 +13,28 @@ namespace ReduceEquations {
         /// </summary>
         public int Row { get { return row; } }
         #endregion
-
         #region メソッド
         /// <summary>
         /// 逆行列を作る
         /// </summary>
         /// <returns>対する逆行列</returns>
         public Matrix Inverse() {
-            int n = row;    //n次正方行列
-            Fraction[,] ext = new Fraction[n, n * 2];        //単位行列を横に置く行列
-            Fraction[] buff_1 = new Fraction[n * 2];        //計算過程の行　引かれる行
-            Fraction[] buff_2 = new Fraction[n * 2];        //引く行
-            Fraction[] buff_res = new Fraction[n * 2];     //引いた結果の行
-
-            if (row != col) 
+            int n = row; //n次正方行列
+            Fraction[,] ext = new Fraction[n, n * 2]; //単位行列を横に置く行列
+            Fraction[] buff_1 = new Fraction[n * 2]; //計算過程の行　引かれる行
+            Fraction[] buff_2 = new Fraction[n * 2]; //引く行
+            Fraction[] buff_res = new Fraction[n * 2]; //引いた結果の行
+            long[] ol_numer = new long[n * 2]; //１行分の分子を取り出す、　公約数で割ってオーバー（アンダー）フロー防止
+            long[] ol_denom = new long[n * 2]; //１行分の分母　〃
+            if (row != col)
                 throw new ArithmeticException("正方でない行列に逆行列は定義されません。");
-
             //拡大行列を作成
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n * 2; j++) {
                     //左側
                     if (j < n) {
                         ext[i, j] = Value[i, j];
-                    } else {    //右の単位行列
+                    } else { //右の単位行列
                         if (i == (j - n))
                             ext[i, j] = (Fraction)1;
                         else
@@ -45,17 +42,16 @@ namespace ReduceEquations {
                     }
                 }
             }
-            //上計算...OK!
+            //上計算...
             for (int j = 0; j < n - 1; j++) {
                 for (int i = j + 1; i < n; i++) {
-
-                    if (ext[j, j].Numerator == 0) {     //行交換
+                    if (ext[j, j].Numerator == 0) { //行交換
                         bool ischange = false;
                         int k = 0;
                         do {
-                            if (ext[k, j].Numerator != 0) {  //0じゃないならここで交換や！
+                            if (ext[k, j].Numerator != 0) { //探した奴が0じゃないならここで交換や！
                                 Fraction[] tmp = new Fraction[n * 2];
-                                for (int kk = 0; kk < n * 2; kk++) 
+                                for (int kk = 0; kk < n * 2; kk++)
                                     tmp[kk] = ext[j, kk];
                                 for (int kk = 0; kk < n * 2; kk++)
                                     ext[j, kk] = ext[k, kk];
@@ -66,33 +62,69 @@ namespace ReduceEquations {
                             k++;
                         } while (!ischange);
                     }
-
+                    //公約数で割る
+                    for (int k = 0; k < n; k++) {
+                        //全部を変数に格納（必要な作業じゃないが、可読性のために）
+                        for (int kk = 0; kk < n * 2; kk++) {
+                            ol_numer[kk] = ext[k, kk].Numerator;
+                            ol_denom[kk] = ext[k, kk].Denominator;
+                        }
+                        //分子の公約数を求める
+                        long nu_gcd = 1; int en = 0; //enは初期化したやつの添字を代入
+                        for (int kk = 0; kk < ol_numer.Length; kk++) {
+                            if (ol_numer[kk] != 0) {
+                                nu_gcd = ol_numer[kk];
+                                en = kk;
+                                break;
+                            }
+                        }
+                        //初期値を元に、gcdを求める（0は除外）
+                        for (int kk = en + 1; kk < ol_numer.Length; kk++) {
+                            if (ol_numer[kk] != 0)
+                                nu_gcd = Euclid(nu_gcd, ol_numer[kk]);
+                        }
+                        //分母の公約数を求める
+                        long de_gcd = 1;
+                        for (int kk = 0; kk < ol_denom.Length; kk++) {
+                            if (ol_denom[kk] != 0) {
+                                de_gcd = ol_denom[kk];
+                                en = kk;
+                                break;
+                            }
+                        }
+                        //初期値を元に、gcdを求める（０は除外）
+                        for (int kk = en + 1; kk < ol_denom.Length; kk++) {
+                            if (ol_denom[kk] != 0)
+                                de_gcd = Euclid(de_gcd, ol_denom[kk]);
+                        }
+                        Fraction fr_gcd = new Fraction(de_gcd, nu_gcd); //gcdの分数（逆数）
+                        //かけることでいい感じになる
+                        for (int kk = 0; kk < n * 2; kk++)
+                            ext[k, kk] = (ext[k, kk] * fr_gcd).Irreducible();
+                    }
                     //計算
                     for (int k = 0; k < n * 2; k++) {
-                            buff_1[k] = (ext[j, k] * ext[i, j]).Irreducible();
-                            buff_2[k] = (ext[i, k] * ext[j, j]).Irreducible();
-                            buff_res[k] = buff_1[k] - buff_2[k];
+                        buff_1[k] = (ext[j, k].Irreducible() * ext[i, j].Irreducible()).Irreducible();
+                        buff_2[k] = (ext[i, k].Irreducible() * ext[j, j].Irreducible()).Irreducible();
+                        buff_res[k] = (buff_1[k] - buff_2[k]).Irreducible();
                     }
                     for (int k = 0; k < n * 2; k++) {
-                            buff_res[k] = buff_res[k].Irreducible();
-                            ext[i, k] = buff_res[k];
+                        buff_res[k] = buff_res[k].Irreducible();
+                        ext[i, k] = buff_res[k];
                     }
                 }
             }
-
             if (ext[n - 1, n - 1].Numerator == 0) {
                 throw new ArithmeticException("行列は正則ではありません。");
             }
-
             //下計算・・・
             for (int j = n - 1; j > 0; j--) {
                 for (int i = j - 1; i >= 0; i--) {
-
-                    if (ext[j, j].Numerator == 0) {     //行交換
+                    if (ext[j, j].Numerator == 0) { //行交換
                         bool ischange = false;
                         int k = 0;
                         do {
-                            if (ext[k, j].Numerator != 0) {  //0じゃないならここで交換や！
+                            if (ext[k, j].Numerator != 0) { //探した奴が0じゃないならここで交換や！
                                 Fraction[] tmp = new Fraction[n * 2];
                                 for (int kk = 0; kk < n * 2; kk++)
                                     tmp[kk] = ext[j, kk];
@@ -105,42 +137,75 @@ namespace ReduceEquations {
                             k++;
                         } while (!ischange);
                     }
-
+                    //公約数で割る
+                    for (int k = 0; k < n; k++) {
+                        //全部を変数に格納（必要な作業じゃないが、可読性のために）
+                        for (int kk = 0; kk < n * 2; kk++) {
+                            ol_numer[kk] = ext[k, kk].Numerator;
+                            ol_denom[kk] = ext[k, kk].Denominator;
+                        }
+                        //分子の公約数を求める
+                        long nu_gcd = 1; int en = 0; //enは初期化したやつの添字を代入
+                        for (int kk = 0; kk < ol_numer.Length; kk++) {
+                            if (ol_numer[kk] != 0) {
+                                nu_gcd = ol_numer[kk];
+                                en = kk;
+                                break;
+                            }
+                        }
+                        //初期値を元に、gcdを求める（0は除外）
+                        for (int kk = en + 1; kk < ol_numer.Length; kk++) {
+                            if (ol_numer[kk] != 0)
+                                nu_gcd = Euclid(nu_gcd, ol_numer[kk]);
+                        }
+                        //分母の公約数を求める
+                        long de_gcd = 1;
+                        for (int kk = 0; kk < ol_denom.Length; kk++) {
+                            if (ol_denom[kk] != 0) {
+                                de_gcd = ol_denom[kk];
+                                en = kk;
+                                break;
+                            }
+                        }
+                        //初期値を元に、gcdを求める（０は除外）
+                        for (int kk = en + 1; kk < ol_denom.Length; kk++) {
+                            if (ol_denom[kk] != 0)
+                                de_gcd = Euclid(de_gcd, ol_denom[kk]);
+                        }
+                        Fraction fr_gcd = new Fraction(de_gcd, nu_gcd); //gcdの分数（逆数）
+                        //かけることでいい感じになる
+                        for (int kk = 0; kk < n * 2; kk++)
+                            ext[k, kk] = (ext[k, kk] * fr_gcd).Irreducible();
+                    }
                     //計算
                     for (int k = 0; k < n * 2; k++) {
-                            buff_1[k] = (ext[j, k] * ext[i, j]).Irreducible();
-                            buff_2[k] = (ext[i, k] * ext[j, j]).Irreducible();
-                            buff_res[k] = buff_1[k] - buff_2[k];
+                        buff_1[k] = (ext[j, k].Irreducible() * ext[i, j].Irreducible()).Irreducible();
+                        buff_2[k] = (ext[i, k].Irreducible() * ext[j, j].Irreducible()).Irreducible();
+                        buff_res[k] = (buff_1[k] - buff_2[k]).Irreducible();
                     }
                     for (int k = 0; k < n * 2; k++) {
-                            buff_res[k] = buff_res[k].Irreducible();
-                            ext[i, k] = buff_res[k];
+                        buff_res[k] = buff_res[k].Irreducible();
+                        ext[i, k] = buff_res[k];
                     }
-                    (new Matrix(ext)).Show();
-                    Console.WriteLine();
                 }
             }
-
             //割る
             for (int i = 0; i < n; i++) {
                 Fraction rec = ext[i, i].Reciprocal();
-                for(int k = 0; k < n * 2; k++){
-                    ext[i, k] = ext[i,k] * rec;
+                for (int k = 0; k < n * 2; k++) {
+                    ext[i, k] = ext[i, k] * rec;
                     ext[i, k] = ext[i, k].Irreducible();
                 }
             }
-
             //逆行列部分の取り出し
             Fraction[,] result = new Fraction[n, n];
             for (int i = 0; i < n; i++) {
                 for (int j = n; j < n * 2; j++) {
-                        result[i, j-n] = ext[i, j];
+                    result[i, j - n] = ext[i, j];
                 }
             }
-
             return new Matrix(result);
         }
-
         /// <summary>
         /// コンソール上に表示
         /// </summary>
@@ -155,12 +220,11 @@ namespace ReduceEquations {
                 Console.WriteLine();
             }
         }
-
         /// <summary>
         /// ２数のgcdを求める
         /// </summary>
         private long Euclid(long a, long b) {
-            long big, small, r;   //大きいのがbig rはあまり
+            long big, small, r; //大きいのがbig rはあまり
             a = Math.Abs(a);
             b = Math.Abs(b);
             if (a > b) {
@@ -168,29 +232,23 @@ namespace ReduceEquations {
             } else {
                 big = b; small = a;
             }
-
             r = big % small;
-
             if (r == 0)
                 return small;
             else
                 return Euclid(small, r);
         }
         #endregion
-
         #region 演算子、今はまだ実装せず
         public static Matrix operator +(Matrix obj1, Matrix obj2) { return new Matrix(); }
         public static Matrix operator -(Matrix obj1, Matrix obj2) { return new Matrix(); }
         public static Matrix operator -(Matrix obj) { return new Matrix(); }
-
-        public static Matrix operator *(Matrix obj1, Matrix obj2) { 
-            return new Matrix(); 
+        public static Matrix operator *(Matrix obj1, Matrix obj2) {
+            return new Matrix();
         }
-
         public static Matrix operator *(Fraction obj1, Matrix obj2) { return new Matrix(); }
         public static Matrix operator *(Matrix obj1, Fraction obj2) { return new Matrix(); }
         #endregion
-
         #region コンストラクタ
         /// <summary>
         /// 2*2単位行列を生成
@@ -206,21 +264,18 @@ namespace ReduceEquations {
                 }
             }
         }
-
         /// <summary>
         /// 任意の大きさの行列を生成。要素は全て0
         /// </summary>
         /// <param name="r">行数</param>
         /// <param name="colmun">列数</param>
-        public Matrix(int r, int c) { 
-            Value = new Fraction[r,c];
+        public Matrix(int r, int c) {
+            Value = new Fraction[r, c];
             row = r; col = c;
             for (int i = 0; i < r; i++)
                 for (int j = 0; j < c; j++)
                     this.Value[i, j] = (Fraction)0;
         }
-
-
         /// <summary>
         /// 任意の行列を生成
         /// </summary>
@@ -229,7 +284,6 @@ namespace ReduceEquations {
             Value = value;
             row = value.GetLength(0); col = value.GetLength(1);
         }
-
         /// <summary>
         /// 任意の行列を生成
         /// </summary>
@@ -240,7 +294,6 @@ namespace ReduceEquations {
             Value = new Fraction[r, c];
             row = r; col = c;
             int counter = 0;
-
             for (int i = 0; i < r; i++) {
                 for (int j = 0; j < c; j++) {
                     Value[i, j] = value[counter];
--- ReduceEquations/Program.cs
+++ ReduceEquations/Program.cs
@@ -3,11 +3,97 @@
 namespace ReduceEquations {
     class Program {
         static void Main(string[] args) {
-            Matrix a = new Matrix(2, 2,     1, 2,
-                                                        4, 7);
-            a = a.Inverse();
-            a.Show();
-            Console.ReadLine();
+            Console.WriteLine("逆行列を計算するプログラムです。");
+            String yn = null;
+            do {
+                int n = 0;
+                do {
+                    Console.Write("n * n ? (nは2~5) >>");
+                    try {
+                        n = int.Parse(Console.ReadLine());
+                    } catch (Exception) {
+                        Console.WriteLine("ちゃんと数を入力しようね！");
+                    }
+                } while (n <= 1 || n > 5);
+
+                Fraction[,] fr = new Fraction[n, n];
+                Console.WriteLine();
+                Console.WriteLine("今から行列を入力してください。");
+                Console.WriteLine("行列の入力方法は１行ずつです。ただし要素の間は半角スペースをいれる。分数の場合は/（半角）を使用いることで表現できる。");
+                for (int i = 0; i < n; i++) {
+                    Console.Write((i + 1) + "行目 >>");
+                    String rowstr = Console.ReadLine();
+                    Fraction[] rowfr = new Fraction[n];
+                    try {
+                        rowfr = ToRow(rowstr, n);
+                    } catch (Exception) {
+                        Console.WriteLine("要素が足りません。もう一度入力してください。");
+                        i--; continue;
+                    }
+                    for (int k = 0; k < n; k++)
+                        fr[i, k] = rowfr[k];
+                }
+
+                Matrix mr = new Matrix(fr);
+                try {
+                    mr = mr.Inverse();
+                    mr.Show();
+                } catch (Exception) {
+                    Console.WriteLine("行列は正則ではありません。");
+                }
+
+                Console.WriteLine();
+                Console.Write("やめますか？(y/n) >");
+                yn = Console.ReadLine();
+            } while (yn[0] != 'y');
+        }
+
+        /// <summary>
+        /// 受け取った文字列を行列の行１つ分の配列にして返す。
+        /// </summary>
+        /// <param name="str">変換する文字列</param>
+        /// <returns>一行</returns>
+        static public Fraction[] ToRow(String str, int col) {
+            Fraction[] result = new Fraction[col];      //返す配列
+
+            //スペースが要素分あるかどうか確認
+            int last_indexer = 0;
+            for (int i = 0; i < col - 1; i++) {
+                last_indexer = str.IndexOf(' ', last_indexer);     //スペースのあるインデックス
+                if (last_indexer == -1) {
+                    throw new Exception();
+                }
+            }
+
+            //実際に文字列から要素を取り出す。
+            for (int i = 0; i < col; i++) {
+                long numer, denom;
+                int sp_in = str.IndexOf(' ');     //スペースのあるインデックス
+                String value = null;
+
+                /* "a b c"→"a "を取得して"b c"に切り取る。このような操作 */
+                //スペースがあるときの処理
+                if (sp_in != -1) {
+                    value = str.Substring(0, sp_in + 1);
+                    str = str.Remove(0, sp_in + 1);
+                } else {    //ないときの処理
+                    value = str;
+                }
+
+
+                int sla_in = value.IndexOf('/');
+                if (sla_in == -1) {                 //切り取った要素が分数じゃない場合
+                    numer = long.Parse(value);
+                    denom = 1;
+                } else {        //分数の時
+                    numer = long.Parse(value.Substring(0, sla_in));
+                    denom = long.Parse(value.Substring(sla_in + 1));
+                }
+
+                result[i] = new Fraction(numer, denom);
+            }
+
+            return result;
         }
     }
 }
